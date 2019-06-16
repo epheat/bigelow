65301c7c6193c0f9fbfafaef19bba2c4c58421d3
scottstav <scottstavinoha@gmail.com>
2019-06-16 14:06:18 -0700

    add s3 and dotenv to package config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 var S3Plugin = require('webpack-s3-plugin')
-var dotenv = require('dotenv').config({path: __dirname + '/.env'});
+var dotenv = require('dotenv').config({
+    path: __dirname + '/.env'
+});
 
 module.exports = {
     entry: './src/index.js',
@@ -41,8 +43,7 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
-            },
-            {
+            }, {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             },
@@ -50,11 +51,11 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [{
-		    loader: 'file-loader',
-		    options: {
-			publicPath: 'https://s3.amazonaws.com/bglw.org/my-dist/dist/'
-		    },
-		}],
+                    loader: 'file-loader',
+                    options: {
+                        publicPath: 'https://s3.amazonaws.com/bglw.org/my-dist/dist/'
+                    }
+                }]
             }
         ]
     },
@@ -68,35 +69,34 @@ module.exports = {
             filename: './index.html',
         }),
         new S3Plugin({
-    	    s3Options: {
-    		accessKeyId: process.env.API_ACCESS_KEY, // Your AWS access key
-    		secretAccessKey: process.env.API_SECRET_ACCESS_KEY, // Your AWS secret key
-    		region: 'us-east-1' // The region of your S3 bucket
-    	    },
-    	    s3UploadOptions: {
-    		Bucket: 'bglw.org', // Your bucket name
-    		// Here we set the Content-Encoding header for all the gzipped files to 'gzip'
-    		ContentEncoding(fileName) {
-    		    if (/\.gz/.test(fileName)) {
-    			return 'gzip'
-    		    }
-    		},
-    		// Here we set the Content-Type header for the gzipped files to their appropriate values, so the browser can interpret them properly
-    		ContentType(fileName) {
-    		    if (/\.css/.test(fileName)) {
-    			return 'text/css'
-    		    }
-    		    if (/\.js/.test(fileName)) {
-    			return 'text/javascript'
-    		    }
-    		}
-    	    },
-    	    basePath: 'my-dist', // This is the name the uploaded directory will be given
-    	    directory: './dist' // This is the directory you want to upload
-        }
-    ),
-	new webpack.DefinePlugin({
-	    "process.env": dotenv.parsed
-	})
+            s3Options: {
+                accessKeyId: process.env.API_ACCESS_KEY, // Your AWS access key
+                secretAccessKey: process.env.API_SECRET_ACCESS_KEY, // Your AWS secret key
+                region: 'us-east-1' // The region of your S3 bucket
+            },
+            s3UploadOptions: {
+                Bucket: 'bglw.org', // Your bucket name
+                // Here we set the Content-Encoding header for all the gzipped files to 'gzip'
+                ContentEncoding(fileName) {
+                    if (/\.gz/.test(fileName)) {
+                        return 'gzip'
+                    }
+                },
+                // Here we set the Content-Type header for the gzipped files to their appropriate values, so the browser can interpret them properly
+                ContentType(fileName) {
+                    if (/\.css/.test(fileName)) {
+                        return 'text/css'
+                    }
+                    if (/\.js/.test(fileName)) {
+                        return 'text/javascript'
+                    }
+                }
+            },
+            basePath: 'my-dist', // This is the name the uploaded directory will be given
+            directory: './dist' // This is the directory you want to upload
+        }),
+        new webpack.DefinePlugin({
+            "process.env": dotenv.parsed
+        })
     ]
 };
