0bba890007fd7d661c101d5bb9e1fcb9c944edcf
Jack Stenglein <jackstenglein@utexas.edu>
2019-06-23 01:43:43 -0700

    Add PokerResults page

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Tub from 'Assets/tub.jpg';
 import Poker from 'Assets/2_7.png';
@@ -11,6 +12,7 @@ const Events = () => {
             <img src={Tub} />
             <h2>Poker Night</h2>
             <p>Every Wednesday we realize we forgot to invite people over for poker night so we just play between the 4 of us it's great.</p>
+            <p>View the <Link to='/poker'>results</Link> from past games.</p>
             <img src={Poker} />
         </div>
     );
